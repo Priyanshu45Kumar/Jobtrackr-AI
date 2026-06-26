907163b93c7918fd18c1b1d80445fca86776e086
Priyanshu45Kumar <py297065@gmail.com>
2026-06-26 14:10:28 +0530

    Fixed backend CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -18,20 +18,14 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL,
-];
+
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL,
+    ],
     credentials: true,
   })
 );

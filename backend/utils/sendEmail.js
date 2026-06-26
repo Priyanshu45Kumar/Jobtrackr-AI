e13622756f45e6d34c6b9fc3a4be607776e2ef26
Priyanshu45Kumar <py297065@gmail.com>
2026-06-26 19:10:59 +0530

    Fixed Gmail SMTP email configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,11 +2,18 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
   });
 
   await transporter.sendMail({
